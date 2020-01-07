0e96f11e4949efd5e690fd93c34ef1a9355281d3
alexooh <alexandero2020@headroyce.org>
2020-01-07 10:41:57 -0800

    first git commit

---
+++ start.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__() # just run the init of parent class (nn.Module)
+        self.conv1 = nn.Conv2d(1, 32, 5) # input is 1 image, 32 output channels, 5x5 kernel / window
+        self.conv2 = nn.Conv2d(32, 64, 5) # input is 32, bc the first layer output 32. Then we say the output will be 64 channels, 5x5 conv
+        self.conv3 = nn.Conv2d(64, 128, 5)
+
+x= torch.randn(50,50).view(-1,1,50,50)
+self._to_linear = None
+self.convs(x)
+
+
