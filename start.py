0ce8987a0ed1e45d7f1c291065b09f0c2a0c23cb
alexooh <alexandero2020@headroyce.org>
2020-01-08 14:25:18 -0800

    committt`

--- start.py
+++ start.py
@@ -1,16 +1,24 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
+import numpy as np
+import math
 class Net(nn.Module):
-    def __init__(self):
-        super().__init__() # just run the init of parent class (nn.Module)
-        self.conv1 = nn.Conv2d(1, 32, 5) # input is 1 image, 32 output channels, 5x5 kernel / window
-        self.conv2 = nn.Conv2d(32, 64, 5) # input is 32, bc the first layer output 32. Then we say the output will be 64 channels, 5x5 conv
-        self.conv3 = nn.Conv2d(64, 128, 5)
+     def __init__(self, x, y):
+        self.input      = x
+        #starting value of weights doesn't matter as long as
+        self.weights1   = np.random.rand(self.input.shape[1],4) 
+        self.weights2   = np.random.rand(4,1)                 
+        self.y          = y
+        self.output     = np.zeros(self.y.shape)
+    def feedforward(self):
+        #1/(1+e^-x) sigmoid function
+        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
+        self.output = sigmoid(np.dot(self.layer1, self.weights2))
 
-x= torch.randn(50,50).view(-1,1,50,50)
-self._to_linear = None
-self.convs(x)
+    def backprop(self):
+        # apply chain rule to find derivative of the loss function with respect to weights2 and weights1
+        d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
+        d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
 
-
+        # add or subtract off of weights based on derivative
+        self.weights1 += d_weights1
+        self.weights2 += d_weights2
+    
